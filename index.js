69faa5818b73fcfc4eb4eb8cea2ebf740c78f93f
Pau <frikeldon@gmail.com>
2021-08-11 01:40:32 +0200

    Metodo para comparar valores OData

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ export { default as OdataDate } from './lib/date.js'
 export { default as OdataNumber } from './lib/number.js'
 export { default as OdataLookup } from './lib/lookup.js'
 
+export { default as odataEquals } from './lib/equals.js'
 export { default as odataFilter } from './lib/filter.js'
 
 export { default as Odata } from './lib/odata.js'
